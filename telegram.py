8e0b4f00b9a7f5905548c2b3cf0752af15620dcf
dagoberto <dagobertomedeiros@gmail.com>
2021-06-11 00:46:29 -0300

    inserindo idChat e alterando a logica

--- telegram.py
+++ telegram.py
@@ -5,15 +5,23 @@ from datetime import datetime
 
 
 #Parametros
+<<<<<<< HEAD
+api_id = xxxx #dados fornecidos pelo telegram
+api_hash = 'xxxx' #dados fornecidos pelo telegram
+txtSearch = ['Boa noite', 'Bom dia'] #defina um conjunto chave de caracteres que devem ser buscados entre as mensagens
+numLimit = 15 #Sempre utilizar 1, evita retrabalho com mensagens duplicadas, assim a coleta será sempre da ultima mensagem
+chatOrGroup = None #coloca o nome do grupo ou chat_id do grupo
+=======
 api_id = XXXXX #dados fornecidos pelo telegram.
 api_hash = 'XXXXXX' #dados fornecidos pelo telegram.
 txtSearch = ['closed'] #defina um conjunto chave de expressões que devem ser buscados entre as mensagens. Devem estar em aspas e numa lista ['xxx', 'yyy', 'cccc'].
 numLimit = 5 #Este número determina a quantidade de mensagens que serão coletadas a cada consulta ao seu telegram.
 chatOrGroup = None #'None' para pesquisar em todos os grupos/chats/contatos ou coloca o nome do grupo ou chat_id do grupo
+>>>>>>> 7185da68911a774d6ced054c52440de714b55cb1
 delay = 5 #defina em segundos o tempo de espera entre uma execução e outra das buscas por novas mensagens
+listIdChat = []#defina uma lista com os ID's dos grupos e canais que aceitará gravar as mensagens ou deixe a lista vazia
 conexao = pymysql.connect(db='telagram_mensagens', user='root', passwd='test') #dados conexão com o BD
-
-queryInsert = 'INSERT INTO telegram_message (IDMESSAGE, TXTMESSAGE, DATEINSERT) VALUES (%s, %s, %s)' #query para insert de dados
+queryInsert = 'INSERT INTO telegram_message (IDMESSAGE, TXTMESSAGE, DATEINSERT, IDCHAT) VALUES (%s, %s, %s, %s)' #query para insert de dados
 querySelect = 'SELECT IDMESSAGE FROM telagram_mensagens.telegram_message' #query para consultar ids já cadastrados no BD
 
 
@@ -35,29 +43,35 @@ client.start()
 idTemp = []
 countChat = 0
 countSearch = 0
-countLimit = 0
+
 while(True):
     numSearch = len(txtSearch)
     while(countSearch < numSearch):
-        message = client.get_messages(entity=chatOrGroup, search=txtSearch[countSearch], limit=numLimit)
-        message = list(message)
-        while(countLimit<numLimit):
-            strMessage = str(message[countLimit])
-            txtMessage = strMessage.split('message=')
-            idMessage = strMessage.split('Message(id=')
-            idMessage = idMessage[1][0:4]
-            txtMessage = txtMessage[1].split(', out=')
-            txtMessage = txtMessage[0]
-            if idMessage not in idTemp and idMessage not in listBD:
-                now = datetime.now()
-                sqlDados = (idMessage, txtMessage, now)
-                cursor.execute(queryInsert, sqlDados)
-                conexao.commit()
-                conexao.close()
-                numRegistros += 1
-                countLimit+=1
-                idTemp.append(idMessage)
-                print('Total de registros inseridos - ', numRegistros,'\n')
+        for message in client.get_messages(entity=chatOrGroup, search=txtSearch[countSearch], limit=numLimit):
+            txtMessage = message.message
+            idMessage = message.id
+            idChat = message.peer_id
+            dateMsg = message.date
+            if (idMessage not in idTemp) and (idMessage not in listBD):
+                if len(listIdChat) > 0:
+                    if idChat in listIdChat:
+                        sqlDados = (idMessage, txtMessage, dateMsg, idChat)
+                        conexao.ping(reconnect=True)
+                        cursor.execute(queryInsert, sqlDados)
+                        conexao.commit()
+                        conexao.close()
+                        numRegistros += 1
+                        idTemp.append(idMessage)
+                        print('Total de registros inseridos -> ', numRegistros,'\n')
+                elif len(listIdChat) == 0:
+                    sqlDados = (idMessage, txtMessage, dateMsg, idChat)
+                    conexao.ping(reconnect=True)
+                    cursor.execute(queryInsert, sqlDados)
+                    conexao.commit()
+                    conexao.close()
+                    numRegistros += 1
+                    idTemp.append(idMessage)
+                    print('Total de registros inseridos -> ', numRegistros,'\n')
         countSearch+=1
+    print('Script em execução....', datetime.now())
     time.sleep(delay)
-
